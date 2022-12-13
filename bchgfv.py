3814f54e3d770850029ce7bb1f31f299d8547bf3
monica-1824 <120354267+monica-1824@users.noreply.github.com>
2022-12-13 11:37:06 +0530

    Add files via upload

---
+++ bchgfv.py
@@ -0,0 +1,9 @@
+list=[1,56,89,28]
+print("the maximum number is ",max(list))
+print("the maximum number is ",min(list))
+sum=max(list)+min(list)
+print("sum is",sum)
+difference=max(list)-min(list)
+print("difference is",difference)
+product=max(list)*min(list)
+print("product is ",product)
